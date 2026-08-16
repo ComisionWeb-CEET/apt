ae48e27d1f9ed39e778d8541e9ccc9b0c85a4022
Raúl <raul.mgherman@alumnos.upm.es>
2026-08-16 15:35:17 +0200

    arreglo sockets pt4

--- frontend/src/config/socket.js
+++ frontend/src/config/socket.js
@@ -1,8 +1,7 @@
 import { io } from "socket.io-client";
 
-// Usamos la variable para detectar si estamos en local o en producción
-const url = import.meta.env.DEV ? "http://localhost:8080" : "https://pideturno.ceet.org.es";
+const url = import.meta.env.DEV ? "http://localhost:8080" : "http://20.111.41.211:8080";
 
 export const socket = io(url, {
-  transports: ["polling"]
+  transports: ["websocket"] 
 });
