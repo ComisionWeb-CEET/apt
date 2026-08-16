2a97d5f76ed2183f5eafd77d5d4dc7e9cbe2c596
Raúl <raul.mgherman@alumnos.upm.es>
2026-08-16 15:19:55 +0200

    arreglo error io no definido

--- server/server.js
+++ server/server.js
@@ -38,6 +38,7 @@ app.get(/.*/, (req, res) => {
 });
 
 const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
 
 const allowedOrigins = [
   'http://localhost:5173',  // Development
